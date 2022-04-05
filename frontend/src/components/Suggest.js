3239f5f82b4195630daf44f4cc24efccf3379eef
ge-nome <ebubeokerezi@gmail.com>
2022-04-05 11:19:58 +0100

    My update

--- frontend/src/components/Suggest.js
+++ frontend/src/components/Suggest.js
@@ -1,16 +1,13 @@
-import {useState} from "react";
-import { Link, } from "react-router-dom";
-import {AiOutlinePlaySquare} from "react-icons/ai"
+// import {useState} from "react";
+// import { Link, } from "react-router-dom";
+import {AiOutlineFileSearch} from "react-icons/ai"
 
 function Suggest(props) {
     console.log(props)
     return (
-        <div className="suggestions">
-            <div className="image">
-                <img className="sugg-img" src="image/uimg.jpg" alt=""></img>
-            </div>
-            <div className="sugg-title">{props.title_1} ({props.title_2})</div>
-            <div className="sugg-icon"><AiOutlinePlaySquare size={20}/></div>
+        <div className="suggestions">            
+            <div className="sugg-title">{props.title_1} {props.title_2}</div>
+            <div className="sugg-icon"><AiOutlineFileSearch size={20}/></div>
         </div>
        
         );
